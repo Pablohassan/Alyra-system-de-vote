a94006c98a49be57a715aa325a0048864fa1e438
Pablohassan <rusmir-sadikovic_student2022@wilder.school>
2022-10-18 13:55:48 +0200

    rendu projet 1

--- Voting.sol
+++ Voting.sol
@@ -40,7 +40,7 @@ contract Voting is Ownable {
     mapping(address => bool) private whitelist;
 
     modifier checkRegistering() {
-        require(whitelist[msg.sender] == true, "you are not authorized");
+        require(whitelist[msg.sender] == true, "you are not authorized to participate");
         _;
     }
 
@@ -51,7 +51,7 @@ contract Voting is Ownable {
                 !votants[msg.sender].hasVoted &&
                 workflowStatus == WorkflowStatus.VotingSessionStarted &&
                 workflowStatus != WorkflowStatus.VotingSessionEnded,
-            "Tu ne peut pas voter"
+            "Your can't vote"
         );
         _;
     }
@@ -62,6 +62,7 @@ contract Voting is Ownable {
 
     function changeStatus() public onlyOwner {
         adminWhitelist(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
+        require (block.timestamp >= 2 minutes);
         workflowStatus = WorkflowStatus.ProposalsRegistrationStarted;
         emit WorkflowStatusChange(WorkflowStatus(0), WorkflowStatus(1));
     }
@@ -82,10 +83,7 @@ contract Voting is Ownable {
     }
 
     function adminWhitelist(address _voterAddress) public onlyOwner {
-        require(
-            workflowStatus == WorkflowStatus.RegisteringVoters ||
-                workflowStatus == WorkflowStatus.ProposalsRegistrationStarted
-        );
+        require(workflowStatus == WorkflowStatus.RegisteringVoters || workflowStatus == WorkflowStatus.ProposalsRegistrationStarted);
         require(
             !whitelist[_voterAddress],
             "This address is already whitelisted !"
@@ -99,12 +97,12 @@ contract Voting is Ownable {
     {
         require(
             !votants[msg.sender].isRegistered,
-            "Proposition already registreted"
+            "Your proposition si already registreted"
         );
         require(
             (workflowStatus == WorkflowStatus.ProposalsRegistrationStarted &&
                 workflowStatus != WorkflowStatus.ProposalsRegistrationEnded),
-            "Tu ne peut pas faire de proposition pour l'nstant"
+            "You can't do propositions a this time "
         );
 
         for (uint256 i = 0; i < proposalDescription.length; i++) {
@@ -117,7 +115,7 @@ contract Voting is Ownable {
     }
 
     function Vote(uint256 _votedProposalId) external checkRegistering {
-        require(proposals.length > _votedProposalId);
+        require(proposals.length > _votedProposalId, "Please chose existing proposition  ");
         proposals[_votedProposalId].voteCount++;
         votants[msg.sender].hasVoted = true;
         votants[msg.sender].votedProposalId = _votedProposalId;
